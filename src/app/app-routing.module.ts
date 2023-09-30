a20d930e257079562c063cca92f92e655424026b
Flora Esposito <flora.98e@gmail.com>
2023-09-30 16:03:27 +0200

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'private',
+  loadChildren: () => import('./private/private.module').then(m => m.PrivateModule),
+  canActivate: [authGuard]
+},
+{
+  path: 'public',
+  loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+},
+{
+  path: '',
+  redirectTo: 'private',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  redirectTo: 'private',
+  pathMatch: 'full'
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
